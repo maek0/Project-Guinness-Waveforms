ba37ceb11b1f9dfd1b5d48bedacb44c361deb5c6
maek0 <kaitlinkramer014@gmail.com>
2023-09-20 17:38:42 -0500

    fixed voltage check infinite loop.

--- guinnessFilter.py
+++ guinnessFilter.py
@@ -2,20 +2,19 @@ def guinnessFilter():
     import matplotlib.pyplot as plt
     from scipy import signal
     import numpy as np
-    # from CheckFile import CheckFile
-    # from VoltageCheck import VoltageCheck
-    # from linearRegression import linearRegression
     from support_functions import CheckFile, VoltageCheck, linearRegression
     import datetime
+    import fpdf
+    
+    # need matplotlib, scipy, numpy, scikit (pip install numpy scikit-learn statsmodels), fpdf
 
     datetime_rn = datetime.datetime.now()
-    str_datetime_rn = datetime_rn.strftime("%d-%b-%Y, %X %Z")
+    # str_datetime_rn = datetime_rn.strftime("%d-%b-%Y, %X %Z")
 
     filepath = input('Enter the name or file path of the oscilloscope .csv output: ')
     filename = CheckFile(filepath)
     
-    voltageLimit = input('Enter the voltage limit of the Guinness generator of the captured waveform: ')
-    VoltageCheck(voltageLimit)
+    voltageLimit = VoltageCheck()
     
     headers = ["Time", "Voltage"]
     csvFile = open(filepath)
@@ -23,11 +22,7 @@ def guinnessFilter():
     x = csvArray[2:,0]
     y = csvArray[2:,1]
     
-    plt.plot(x,y)
-    plt.title("Guinness Generator Output")
-    plt.xlabel(headers[0])
-    plt.ylabel(headers[1])
-    
+    plt.plot(x,y)  
     # xaxis = range(int(min(x)), int(max(x)), 2)
     # yaxis = range(int(min(y)), int(max(y)), 5)
     # plt.xticks(xaxis)
@@ -46,21 +41,24 @@ def guinnessFilter():
     
     cutoff = 0.66*float(voltageLimit)
     ind = np.where(y_peaks_yvalues>=cutoff)[0][0]
-    # print(ind)
-    # print(y_peaks_yvalues[ind])
     
     fiveVoltRampY = y_peaks_yvalues[:ind]
     fiveVoltRampX = x[y_peaks_xvalues[:ind]]
+    
     twoVoltRampY = y_peaks_yvalues[ind:]
     twoVoltRampX = x[y_peaks_xvalues[ind:]]
     
     fiveV_rsq, fiveV_slope, fiveV_intercept, fiveV_fit = linearRegression(fiveVoltRampX, fiveVoltRampY)
+    
     twoV_rsq, twoV_slope, twoV_intercept, twoV_fit = linearRegression(twoVoltRampX, twoVoltRampY)
     
     plt.plot(x,y, color = 'blue')
-    plt.axhline(y=cutoff, color = 'k', linestyle = '--', label = "{:.2f}V (66%% of Voltage Limit)".format(float(cutoff)))
+    
+    # # plt.axhline(y=cutoff, color = 'k', linestyle = '--', label = "{:.2f}V (66% of Voltage Limit)".format(float(cutoff)))
+    
     plt.scatter(fiveVoltRampX,fiveVoltRampY, color = 'green')
     plt.scatter(twoVoltRampX,twoVoltRampY, color = 'orange')
+    
     plt.title("Guinness Generator Output, Voltage Limit = {}V\nInput file name: '{}'".format(voltageLimit, filename))
     plt.text(min(x),min(y),"{}".format(str_datetime_rn),fontsize="small")
     plt.xlabel(headers[0])
@@ -68,6 +66,7 @@ def guinnessFilter():
     
     plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(fiveV_slope[0], fiveV_intercept, fiveV_rsq))
     plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(twoV_slope[0], twoV_intercept, twoV_rsq))
+    
     plt.xlim(min(x)-1,max(x)+1)
     plt.ylim(min(y)-3,max(y)+3)
     plt.legend(loc="upper left")
--- support_functions.py
+++ support_functions.py
@@ -1,47 +1,62 @@
 import sys
 import os
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 
 def CheckFile(filepath):
     if os.path.exists(filepath):
-        if filepath[-1]=="/":
+        if filepath[-1] == "/":
             filepath = filepath[:-1]
-        elif filepath[-4:]==".csv":
+        elif filepath[-4:] == ".csv":
             print("File found. File is a .csv file.")
             return os.path.basename(filepath)
         else:
-            print("File found, but it is not a .csv file. Double check input file type and name.")
+            print(
+                "File found, but it is not a .csv file. Double check input file type and name."
+            )
+            sys.exit()
     else:
         print("File was not found. Double check input file path and file name.")
         sys.exit()
 
-def linearRegression(x,y):
-    import numpy as np
-    from sklearn.linear_model import LinearRegression
-    
-    x = np.array(x).reshape((-1,1))
-    
-    model = LinearRegression().fit(x,y)
-    r_sq = model.score(x,y)
+
+def linearRegression(x, y):
+    x = np.array(x).reshape((-1, 1))
+
+    model = LinearRegression().fit(x, y)
+    r_sq = model.score(x, y)
     slope = model.coef_
     intercept = model.intercept_
     y_predict = model.predict(x)
-    
+
     return r_sq, slope, intercept, y_predict
 
-def VoltageCheck(voltageLimit):
+
+def VoltageCheck():
     while True:
+        voltageLimit = input(
+            "Enter the voltage limit of the Guinness generator of the captured waveform: "
+        )
         try:
             voltageLimit = int(voltageLimit)
-            if type(voltageLimit)==str:
+            if type(voltageLimit) == str:
                 raise TypeError
-            elif voltageLimit>150 or voltageLimit<0:
+            elif voltageLimit > 150 or voltageLimit < 0:
                 raise ValueError
-            elif voltageLimit<=150 and voltageLimit>=0:
+            elif voltageLimit <= 150 and voltageLimit >= 0:
+                return voltageLimit
                 break
             else:
                 print("Invalid input.")
                 raise TypeError
         except ValueError:
-            print("Not a valid input. Value must be an integer in the range from 0 to 150.")
+            print(
+                "Not a valid input. Value must be an integer in the range from 0 to 150."
+            )
+            continue
         except TypeError:
-            print("Not a valid input. Value must be an integer in the range from 0 to 150.")
+            print(
+                "Not a valid input. Value must be an integer in the range from 0 to 150."
+            )
+            continue
