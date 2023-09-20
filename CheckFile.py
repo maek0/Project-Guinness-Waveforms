3ba2bada970974705291c5175e733375c0366045
Kaitlin Kramer <kkramer@minnetronixmedical.com>
2023-09-20 14:42:38 -0500

    Fixed plotting results.

--- CheckFile.py
+++ CheckFile.py
@@ -3,8 +3,11 @@ import os
 
 def CheckFile(filepath):
     if os.path.exists(filepath):
-        if filepath[-4:]==".csv":
+        if filepath[-1]=="/":
+            filepath = filepath[:-1]
+        elif filepath[-4:]==".csv":
             print("File found. File is a .csv file.")
+            return os.path.basename(filepath)
         else:
             print("File found, but it is not a .csv file. Double check input file type and name.")
     else:
--- guinnessFilter.py
+++ guinnessFilter.py
@@ -5,10 +5,13 @@ def guinnessFilter():
     from CheckFile import CheckFile
     from VoltageCheck import VoltageCheck
     from linearRegression import linearRegression
-    import os
+    import datetime
+
+    datetime_rn = datetime.datetime.now()
+    str_datetime_rn = datetime_rn.strftime("%d-%b-%Y, %X %Z")
 
     filepath = input('Enter the name or file path of the oscilloscope .csv output: ')
-    CheckFile(filepath)
+    filename = CheckFile(filepath)
     
     voltageLimit = input('Enter the voltage limit of the Guinness generator of the captured waveform: ')
     VoltageCheck(voltageLimit)
@@ -24,20 +27,20 @@ def guinnessFilter():
     plt.xlabel(headers[0])
     plt.ylabel(headers[1])
     
-    xaxis = range(int(min(x)), int(max(x)), 2)
-    yaxis = range(int(min(y)), int(max(y)), 5)
-    plt.xticks(xaxis)
-    plt.yticks(yaxis)
+    # xaxis = range(int(min(x)), int(max(x)), 2)
+    # yaxis = range(int(min(y)), int(max(y)), 5)
+    # plt.xticks(xaxis)
+    # plt.yticks(yaxis)
     
-    plt.show()
+    # plt.show()
     
     y_peaks_xvalues, ypeak_properties = signal.find_peaks(y, height=5,prominence=15,distance=50)
     y_peaks_yvalues = ypeak_properties["peak_heights"]
     # the distance likely will have to change when the frequency is fixed to be 2Hz
     
-    plt.scatter(x[y_peaks_xvalues],y[y_peaks_xvalues])
-    plt.plot(x,y)
-    plt.show()
+    # plt.scatter(x[y_peaks_xvalues],y[y_peaks_xvalues])
+    # plt.plot(x,y)
+    # plt.show()
     # this plot won't have to stay here, currently being used to validate settings
     
     cutoff = 0.66*float(voltageLimit)
@@ -54,17 +57,21 @@ def guinnessFilter():
     twoV_rsq, twoV_slope, twoV_intercept, twoV_fit = linearRegression(twoVoltRampX, twoVoltRampY)
     
     plt.plot(x,y, color = 'blue')
-    plt.axhline(y=cutoff, color = 'r', linestyle = '--', label = "Set Voltage Limit")
+    plt.axhline(y=cutoff, color = 'k', linestyle = '--', label = "{:.2f}V (66%% of Voltage Limit)".format(float(cutoff)))
     plt.scatter(fiveVoltRampX,fiveVoltRampY, color = 'green')
     plt.scatter(twoVoltRampX,twoVoltRampY, color = 'orange')
-    plt.title("Guinness Generator Output\nInput file name: '{}'".format(os.path.basename(filepath)))
+    plt.title("Guinness Generator Output, Voltage Limit = {}V\nInput file name: '{}'".format(voltageLimit, filename))
+    plt.text(min(x),min(y),"{}".format(str_datetime_rn),fontsize="small")
     plt.xlabel(headers[0])
     plt.ylabel(headers[1])
     
-    plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(fiveV_slope), float(fiveV_intercept), float(fiveV_rsq)))
-    plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(twoV_slope), float(twoV_intercept), float(twoV_rsq)))
-    
-    plt.legend()
+    # plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(fiveV_slope), float(fiveV_intercept), float(fiveV_rsq)))
+    # plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(twoV_slope), float(twoV_intercept), float(twoV_rsq)))
+    plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(fiveV_slope[0], fiveV_intercept, fiveV_rsq))
+    plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(twoV_slope[0], twoV_intercept, twoV_rsq))
+    plt.xlim(min(x)-1,max(x)+1)
+    plt.ylim(min(y)-3,max(y)+3)
+    plt.legend(loc="upper left")
     
     plt.show()
     
