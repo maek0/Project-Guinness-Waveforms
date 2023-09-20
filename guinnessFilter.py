8d5a30cbf682d01fb318f514e4d9c19e0ea14bf5
Kaitlin Kramer <kkramer@minnetronixmedical.com>
2023-09-20 13:34:17 -0500

    Added linear regression to the two ramp rate sections. Plotting results.

--- guinnessFilter.py
+++ guinnessFilter.py
@@ -2,16 +2,16 @@ def guinnessFilter():
     import matplotlib.pyplot as plt
     from scipy import signal
     import numpy as np
+    from CheckFile import CheckFile
+    from VoltageCheck import VoltageCheck
+    from linearRegression import linearRegression
     import os
-    import sys
-    import CheckFile
-    import VoltageCheck
 
     filepath = input('Enter the name or file path of the oscilloscope .csv output: ')
-    CheckFile.CheckFile(filepath)
+    CheckFile(filepath)
     
     voltageLimit = input('Enter the voltage limit of the Guinness generator of the captured waveform: ')
-    VoltageCheck.VoltageCheck(voltageLimit)
+    VoltageCheck(voltageLimit)
     
     headers = ["Time", "Voltage"]
     csvFile = open(filepath)
@@ -40,21 +40,32 @@ def guinnessFilter():
     plt.show()
     # this plot won't have to stay here, currently being used to validate settings
     
-    # I have the max points... now I need to use the 66%(voltage limit) to divide the points into two sections
-    # the two sections will have a linear fit ==> slope of the two sections
-    
     cutoff = 0.66*float(voltageLimit)
     ind = np.where(y_peaks_yvalues>=cutoff)[0][0]
-    print(ind)
-    print(y_peaks_yvalues[ind])
+    # print(ind)
+    # print(y_peaks_yvalues[ind])
+    
     fiveVoltRampY = y_peaks_yvalues[:ind]
     fiveVoltRampX = x[y_peaks_xvalues[:ind]]
     twoVoltRampY = y_peaks_yvalues[ind:]
     twoVoltRampX = x[y_peaks_xvalues[ind:]]
-
-    plt.scatter(x[y_peaks_xvalues],y[y_peaks_xvalues])
-    plt.plot(x,y)
-    plt.plot(fiveVoltRampX,fiveVoltRampY)
-    plt.plot(twoVoltRampX,twoVoltRampY)
+    
+    fiveV_rsq, fiveV_slope, fiveV_intercept, fiveV_fit = linearRegression(fiveVoltRampX, fiveVoltRampY)
+    twoV_rsq, twoV_slope, twoV_intercept, twoV_fit = linearRegression(twoVoltRampX, twoVoltRampY)
+    
+    plt.plot(x,y, color = 'blue')
+    plt.axhline(y=cutoff, color = 'r', linestyle = '--', label = "Set Voltage Limit")
+    plt.scatter(fiveVoltRampX,fiveVoltRampY, color = 'green')
+    plt.scatter(twoVoltRampX,twoVoltRampY, color = 'orange')
+    plt.title("Guinness Generator Output\nInput file name: '{}'".format(os.path.basename(filepath)))
+    plt.xlabel(headers[0])
+    plt.ylabel(headers[1])
+    
+    plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(fiveV_slope), float(fiveV_intercept), float(fiveV_rsq)))
+    plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(float(twoV_slope), float(twoV_intercept), float(twoV_rsq)))
+    
+    plt.legend()
+    
     plt.show()
+    
 guinnessFilter()
