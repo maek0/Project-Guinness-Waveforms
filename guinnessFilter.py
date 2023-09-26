7f02581added8cff5a81baf26fbb218913944838
Kaitlin Kramer <kkramer@minnetronixmedical.com>
2023-09-26 14:57:32 -0500

    Separated plotting out into support_functions.py

--- guinnessFilter.py
+++ guinnessFilter.py
@@ -1,8 +1,8 @@
 def guinnessFilter():
-    import matplotlib.pyplot as plt
-    from scipy import signal
+    # import matplotlib.pyplot as plt
+    # from scipy import signal
     import numpy as np
-    from support_functions import CheckFile, VoltageCheck, linearRegression
+    from support_functions import CheckFile, VoltageCheck, plotting_peaks
     import datetime
     import fpdf
     
@@ -22,58 +22,6 @@ def guinnessFilter():
     x = csvArray[2:,0]
     y = csvArray[2:,1]
     
-    # plt.plot(x,y)
-    # xaxis = range(int(min(x)), int(max(x)), 2)
-    # yaxis = range(int(min(y)), int(max(y)), 5)
-    # plt.xticks(xaxis)
-    # plt.yticks(yaxis)
-    # plt.show()
-    
-    y_peaks_xvalues, ypeak_properties = signal.find_peaks(y, height=2.5,prominence=15,distance=50)
-    y_peaks_yvalues = ypeak_properties["peak_heights"]
-    
-    # plt.scatter(x[y_peaks_xvalues],y[y_peaks_xvalues])
-    # plt.plot(x,y)
-    # plt.show()
-    # this plot won't have to stay here, currently being used to validate settings
-    
-    first_peakX = x[y_peaks_xvalues[0]]
-    first_peakY = y_peaks_yvalues[0]
-    
-    cutoff = 0.66*float(voltageLimit)
-    ind = np.where(y_peaks_yvalues>=cutoff)[0][0]
-    
-    fiveVoltRampY = y_peaks_yvalues[:ind]
-    fiveVoltRampX = x[y_peaks_xvalues[:ind]]
-    
-    twoVoltRampY = y_peaks_yvalues[ind:]
-    twoVoltRampX = x[y_peaks_xvalues[ind:]]
-    
-    fiveV_rsq, fiveV_slope, fiveV_intercept, fiveV_fit = linearRegression(fiveVoltRampX, fiveVoltRampY)
-    
-    twoV_rsq, twoV_slope, twoV_intercept, twoV_fit = linearRegression(twoVoltRampX, twoVoltRampY)
-    
-    plt.plot(x,y, color = 'blue')
-    
-    # # plt.axhline(y=cutoff, color = 'k', linestyle = '--', label = "{:.2f}V (66% of Voltage Limit)".format(float(cutoff)))
-    
-    plt.scatter(fiveVoltRampX,fiveVoltRampY, color = 'green')
-    plt.scatter(twoVoltRampX,twoVoltRampY, color = 'orange')
-    
-    plt.plot(first_peakX,first_peakY, "x", color = 'black', label = "First Peak = {:.2f}V".format(first_peakY), markersize = 10)
-    
-    plt.title("Guinness Generator Output, Voltage Limit = {}V\nInput file name: '{}'".format(voltageLimit, filename))
-    plt.text(min(x),min(y),"ST-0001-066-101A, {}".format(str_datetime_rn),fontsize="small")
-    plt.xlabel(headers[0])
-    plt.ylabel(headers[1])
-    
-    plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(fiveV_slope[0], fiveV_intercept, fiveV_rsq))
-    plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(twoV_slope[0], twoV_intercept, twoV_rsq))
-    
-    plt.xlim(min(x)-1,max(x)+1)
-    plt.ylim(min(y)-3,max(y)+3)
-    plt.legend(loc="upper left")
-    
-    plt.show()
+    plotting_peaks(x, y, voltageLimit, filename, str_datetime_rn, headers)
     
 guinnessFilter()
--- support_functions.py
+++ support_functions.py
@@ -2,7 +2,8 @@ import sys
 import os
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
+from scipy import signal
+import matplotlib.pyplot as plt
 
 def CheckFile(filepath):
     if os.path.exists(filepath):
@@ -60,3 +61,43 @@ def VoltageCheck():
                 "Not a valid input. Value must be an integer in the range from 0 to 150."
             )
             continue
+
+def plotting_peaks(x, y, voltageLimit, filename, str_datetime_rn, headers):
+    y_peaks_xvalues, ypeak_properties = signal.find_peaks(y, height=2.5,prominence=15,distance=50)
+    y_peaks_yvalues = ypeak_properties["peak_heights"]
+    
+    first_peakX = x[y_peaks_xvalues[0]]
+    first_peakY = y_peaks_yvalues[0]
+    
+    cutoff = 0.66*float(voltageLimit)
+    ind = np.where(y_peaks_yvalues>=cutoff)[0][0]
+    
+    fiveVoltRampY = y_peaks_yvalues[:ind]
+    fiveVoltRampX = x[y_peaks_xvalues[:ind]]
+    
+    twoVoltRampY = y_peaks_yvalues[ind:]
+    twoVoltRampX = x[y_peaks_xvalues[ind:]]
+    
+    fiveV_rsq, fiveV_slope, fiveV_intercept, fiveV_fit = linearRegression(fiveVoltRampX, fiveVoltRampY)
+    twoV_rsq, twoV_slope, twoV_intercept, twoV_fit = linearRegression(twoVoltRampX, twoVoltRampY)
+    
+    plt.plot(x,y, color = 'blue')
+    
+    plt.scatter(fiveVoltRampX,fiveVoltRampY, color = 'green')
+    plt.scatter(twoVoltRampX,twoVoltRampY, color = 'orange')
+    
+    plt.plot(first_peakX,first_peakY, "x", color = 'black', label = "First Peak = {:.2f}V".format(first_peakY), markersize = 10)
+    
+    plt.title("Guinness Generator Output, Voltage Limit = {}V\nInput file name: '{}'".format(voltageLimit, filename))
+    plt.text(min(x),min(y),"ST-0001-066-101A, {}".format(str_datetime_rn),fontsize="small")
+    plt.xlabel(headers[0])
+    plt.ylabel(headers[1])
+    
+    plt.plot(fiveVoltRampX, fiveV_fit, color = 'green', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(fiveV_slope[0], fiveV_intercept, fiveV_rsq))
+    plt.plot(twoVoltRampX, twoV_fit, color = 'orange', label = "y = {:.2f}x + {:.2f}\n$R^2$ = {:.2f}".format(twoV_slope[0], twoV_intercept, twoV_rsq))
+    
+    plt.xlim(min(x)-1,max(x)+1)
+    plt.ylim(min(y)-3,max(y)+3)
+    plt.legend(loc="upper left")
+    
+    plt.show()
